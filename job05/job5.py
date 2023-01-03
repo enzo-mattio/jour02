5a9ab8551be84d4acff26397dae1b274049f7c58
EnzoMattio <113895326+EnzoMattio@users.noreply.github.com>
2023-01-03 13:18:34 +0100

    Job 05 calculette*

---
+++ job05/job5.py
@@ -0,0 +1,13 @@
+def calcule(num1,operator,num2):
+  if operator == "+":
+        return num1 + num2
+  elif operator == "-":
+        return num1 - num2
+  elif operator == "*":
+        return num1 * num2
+  elif operator == "/":
+        return num1 / num2
+  elif operator == "%":
+        return num1 % num2
+
+print(calcule(25,"/",65))
